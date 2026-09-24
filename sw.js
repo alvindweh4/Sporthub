7aff402385f9fc1326729cc109101d5a2fd2c28e
alvindweh4 <alvindweh4@gmail.com>
2026-09-24 11:44:35 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "sporthub-v1";
-const SHELL = ["./", "index.html", "manifest.json", "icon.svg"];
+const CACHE = "sporthub-v2";
+const SHELL = ["./", "index.html", "manifest.json", "icon.svg", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
